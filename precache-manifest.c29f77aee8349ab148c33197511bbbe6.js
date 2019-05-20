3b0fafd58e3eaae71e394128d4cd61544a858754
Jtapsell <johntapsell@windowslive.com>
2019-05-20 23:33:09 +1200

    Updates

---
+++ precache-manifest.c29f77aee8349ab148c33197511bbbe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdd8cc12c22257742823710e5d0c13e",
+    "url": "/mr-store/index.html"
+  },
+  {
+    "revision": "7dd8699ca5058e8432b0",
+    "url": "/mr-store/static/js/2.3ebbafa1.chunk.js"
+  },
+  {
+    "revision": "7093b84986166c99bbb2",
+    "url": "/mr-store/static/js/main.7604dea2.chunk.js"
+  },
+  {
+    "revision": "a790cb6f81f55a85a899",
+    "url": "/mr-store/static/js/runtime~main.2a4252a5.js"
+  },
+  {
+    "revision": "e0f5b90e5b9ee989e54d27e5689f1789",
+    "url": "/mr-store/static/media/classic-tee.e0f5b90e.jpg"
+  }
+]);
